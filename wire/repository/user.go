26cadd08ab37f65c261adb8d7f3286a4315cfb40
ink <1480224563@qq.com>
2024-11-04 17:21:32 +0800

    wire 简单事例

---
+++ wire/repository/user.go
@@ -0,0 +1,14 @@
+package repository
+
+import "SimShare/wire/repository/dao"
+
+type UserRepository struct {
+	dao *dao.UserDao
+}
+
+func NewUserRepository(dao *dao.UserDao) *UserRepository {
+	return &UserRepository{
+		dao: dao,
+	}
+
+}
